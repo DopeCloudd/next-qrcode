7144c3e022f128c58b51a6e5d4c083194df26f35
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-24 13:58:57 +0200

    add credentials option to next auth

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,11 +1,34 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/db";
 
 export const AuthOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Identifiant", type: "text", placeholder: "demo" },
+        password: { label: "Mot de passe", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("/your/endpoint", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
